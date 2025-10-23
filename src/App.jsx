10e13e122872277e9238a4776e99389fb5327a37
joshua <iuttn123@gmail.com>
2025-10-23 17:00:42 +0900

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+
 function App() {
-  return <></>;
+  const [num, setNum] = useState(0);
+
+  return (
+    <>
+      <button onClick={() => setNum((prev) => prev - 10)}>-10</button>
+      <button onClick={() => setNum((prev) => prev - 1)}>-1</button>
+      <div>{num}</div>
+      <button onClick={() => setNum((prev) => prev + 1)}>+1</button>
+      <button onClick={() => setNum((prev) => prev + 10)}>+10</button>
+    </>
+  );
 }
 
 export default App;
